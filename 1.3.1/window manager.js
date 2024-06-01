54bf1892d696483a1bda0f8c993ed7a75c3aa649
MEME-KING16 <115302822+MEME-KING16@users.noreply.github.com>
2024-05-31 22:05:57 -0400

    Added new dev release

--- 1.3.1/window manager.js
+++ 1.3.1/window manager.js
@@ -51,7 +51,8 @@ function dragElement(elmnt) {
         this.id = `${this.name}${this.process}`
         Window.processamt = Window.processamt + 1
         document.body.innerHTML += `<div id="${this.process}"><div id="${this.name}${this.process}"><div id="${this.name}${this.process}header"><button onclick="interpreter.evaluate(interpreter.parse('call close ${this.process}'))">Close</button></div><div id="${this.name}${this.process}body"></div></div></div>`
-        document.getElementById(this.id).style.border = "15"
+        document.getElementById(this.id).style.border = "1"
+        document.getElementById(this.id).style.borderColor = "black"
         dragElement(document.getElementById(`${this.name}${this.process}`))
         updatefm()
     }
